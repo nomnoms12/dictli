e704518b9092990c281c43831d1dba08dff2ca15
nomnoms12 <alexander.ign0918@gmail.com>
2022-05-15 21:06:47 +0300

    Create React app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+</head>
+<body>
+<noscript>You need to enable JavaScript to run this app.</noscript>
+<div id="root"></div>
+<script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
