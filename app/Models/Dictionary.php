5953091b1f44efc8b28487abfba12e7a3c780f77
nomnoms12 <alexander.ign0918@gmail.com>
2022-05-29 22:43:53 +0300

    Add fillable fields

--- app/Models/Dictionary.php
+++ app/Models/Dictionary.php
@@ -10,6 +10,15 @@ class Dictionary extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+    ];
+
     public function translations(): HasMany
     {
         return $this->hasMany(Translation::class);
--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -10,6 +10,16 @@ class Translation extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'en',
+        'ru',
+    ];
+
     public function dictionary(): BelongsTo
     {
         return $this->belongsTo(Dictionary::class);
