f00bccf60fe8066fe109f77ae67360317d9131a4
nomnoms12 <alexander.ign0918@gmail.com>
2022-05-31 23:53:11 +0300

    Add app name to title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body class="d-flex h-100 text-center bg-light">
 <noscript>You need to enable JavaScript to run this app.</noscript>
